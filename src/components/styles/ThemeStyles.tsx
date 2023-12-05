91459f5c1a204208c978c9b3e97dff106291fce5
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2023-12-05 18:58:38 +0300

    => saved

--- src/components/styles/ThemeStyles.tsx
+++ src/components/styles/ThemeStyles.tsx
@@ -1,23 +1,23 @@
 export const ThemeStyles = {
-	// default - dark theme
+  // default - dark theme
 
-	bodyBackgroundColor: {
-		dark: '#2c2c2c',
-		light: '#f5f5f5',
-	},
+  bodyBackgroundColor: {
+    dark: "#2c2c2c",
+    light: "#f5f5f5",
+  },
 
-	cardBackgroundColor: {
-		dark: '#29242452',
-		light: '#fff',
-	},
+  cardBackgroundColor: {
+    dark: "#29242452",
+    light: "#fff",
+  },
 
-	cardFontColor: {
-		light: '#fff',
-		dark: '#000',
-	},
+  cardFontColor: {
+    light: "#fff",
+    dark: "#000",
+  },
 
-	cardButtonColor: {
-		dark: '#acc51f',
-		light: '#4e71fe',
-	},
-}
+  cardButtonColor: {
+    dark: "#acc51f",
+    light: "#4e71fe",
+  },
+};
